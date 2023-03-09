1de8040ea900b3fa00d9821f4c01468468b1ee88
Vibenk <viktor.benkovics@hotmail.com>
2023-03-09 19:47:21 +0100

    Added SQL create script

---
+++ src/Database/eventmgrdb.sql
@@ -0,0 +1,199 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.11.2-MariaDB-1:10.11.2+maria~ubu2204 - mariadb.org binary distribution
+-- Server OS:                    debian-linux-gnu
+-- HeidiSQL Version:             12.3.0.6589
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for EventMgrDB
+DROP DATABASE IF EXISTS `EventMgrDB`;
+CREATE DATABASE IF NOT EXISTS `EventMgrDB` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `EventMgrDB`;
+
+-- Dumping structure for table EventMgrDB.Event
+DROP TABLE IF EXISTS `Event`;
+CREATE TABLE IF NOT EXISTS `Event` (
+  `EventID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(100) NOT NULL,
+  `Date` date DEFAULT curdate(),
+  `Location` varchar(200) DEFAULT NULL,
+  `Description` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`EventID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table EventMgrDB.Event: ~1 rows (approximately)
+INSERT INTO `Event` (`EventID`, `Name`, `Date`, `Location`, `Description`) VALUES
+	(1, 'DÍszvacsora 2023', '2023-03-05', NULL, NULL);
+
+-- Dumping structure for table EventMgrDB.Organization
+DROP TABLE IF EXISTS `Organization`;
+CREATE TABLE IF NOT EXISTS `Organization` (
+  `OrganizationID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Toplevel` bit(1) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `FK_ParentOrganizationID` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`OrganizationID`),
+  KEY `FK_Organization_Organization` (`FK_ParentOrganizationID`),
+  CONSTRAINT `FK_Organization_Organization` FOREIGN KEY (`FK_ParentOrganizationID`) REFERENCES `Organization` (`OrganizationID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table EventMgrDB.Organization: ~8 rows (approximately)
+INSERT INTO `Organization` (`OrganizationID`, `Toplevel`, `Name`, `FK_ParentOrganizationID`) VALUES
+	(1, b'1', 'Belügy', NULL),
+	(2, b'1', 'Külügy', NULL),
+	(3, b'0', 'Külképviseletek', 2),
+	(4, b'0', 'Rendőrség', 1),
+	(5, b'0', 'ORFK', 4),
+	(6, b'0', 'USA', 3),
+	(7, b'0', 'Németország', 3),
+	(8, b'0', 'Igazgatóság', 5),
+	(9, b'1', 'Polgárőrség', NULL);
+
+-- Dumping structure for table EventMgrDB.Person
+DROP TABLE IF EXISTS `Person`;
+CREATE TABLE IF NOT EXISTS `Person` (
+  `PersonID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(100) DEFAULT '',
+  `LastName` varchar(100) DEFAULT '',
+  `Notes` varchar(2000) DEFAULT NULL COMMENT 'Allergy, pereferences, other - free text',
+  PRIMARY KEY (`PersonID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table EventMgrDB.Person: ~3 rows (approximately)
+INSERT INTO `Person` (`PersonID`, `FirstName`, `LastName`, `Notes`) VALUES
+	(1, 'Jakab', 'Gipsz', 'Laktózérzékeny; vegetáriánus'),
+	(2, 'Beáta', 'Nagy', 'Nem fogyaszt cukrot'),
+	(3, 'Ágnes', 'Kovács', 'Gluténérzékeny');
+
+-- Dumping structure for table EventMgrDB.PersonToEvent
+DROP TABLE IF EXISTS `PersonToEvent`;
+CREATE TABLE IF NOT EXISTS `PersonToEvent` (
+  `FK_EventID` int(10) unsigned NOT NULL,
+  `FK_PersonID` int(10) unsigned NOT NULL,
+  `CreatedDate` datetime DEFAULT current_timestamp(),
+  KEY `FK_PersonToEvent_Event` (`FK_EventID`),
+  KEY `FK_PersonToEvent_Person` (`FK_PersonID`),
+  CONSTRAINT `FK_PersonToEvent_Event` FOREIGN KEY (`FK_EventID`) REFERENCES `Event` (`EventID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_PersonToEvent_Person` FOREIGN KEY (`FK_PersonID`) REFERENCES `Person` (`PersonID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table EventMgrDB.PersonToEvent: ~2 rows (approximately)
+INSERT INTO `PersonToEvent` (`FK_EventID`, `FK_PersonID`, `CreatedDate`) VALUES
+	(1, 3, '2023-03-01 22:21:33'),
+	(1, 2, '2023-03-01 21:21:43');
+
+-- Dumping structure for table EventMgrDB.PersonToOrganization
+DROP TABLE IF EXISTS `PersonToOrganization`;
+CREATE TABLE IF NOT EXISTS `PersonToOrganization` (
+  `FK_PersonID` int(10) unsigned NOT NULL,
+  `FK_OrganizationID` int(10) unsigned NOT NULL,
+  `Role` varchar(100) NOT NULL DEFAULT '',
+  KEY `FK_PersonToOrganization_Person` (`FK_PersonID`),
+  KEY `FK_PersonToOrganization_Organization` (`FK_OrganizationID`),
+  CONSTRAINT `FK_PersonToOrganization_Organization` FOREIGN KEY (`FK_OrganizationID`) REFERENCES `Organization` (`OrganizationID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_PersonToOrganization_Person` FOREIGN KEY (`FK_PersonID`) REFERENCES `Person` (`PersonID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table EventMgrDB.PersonToOrganization: ~4 rows (approximately)
+INSERT INTO `PersonToOrganization` (`FK_PersonID`, `FK_OrganizationID`, `Role`) VALUES
+	(1, 5, 'Őrmester'),
+	(2, 6, 'Nagykövet'),
+	(3, 7, 'Titkár'),
+	(1, 9, 'Polgárőrparancsnok');
+
+-- Dumping structure for table EventMgrDB.Users
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE IF NOT EXISTS `Users` (
+  `UserID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `UserName` varchar(100) NOT NULL,
+  `PasswordHash` varchar(100) NOT NULL,
+  `FK_UserTypeID` smallint(5) unsigned NOT NULL DEFAULT 1,
+  PRIMARY KEY (`UserID`),
+  KEY `fk_Users_Roles` (`FK_UserTypeID`),
+  CONSTRAINT `fk_Users_Roles` FOREIGN KEY (`FK_UserTypeID`) REFERENCES `UserType` (`UserTypeID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table EventMgrDB.Users: ~0 rows (approximately)
+
+-- Dumping structure for table EventMgrDB.UserType
+DROP TABLE IF EXISTS `UserType`;
+CREATE TABLE IF NOT EXISTS `UserType` (
+  `UserTypeID` smallint(5) unsigned NOT NULL,
+  `UserTypeName` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`UserTypeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table EventMgrDB.UserType: ~3 rows (approximately)
+INSERT INTO `UserType` (`UserTypeID`, `UserTypeName`) VALUES
+	(1, 'Admin'),
+	(2, 'Felhasználó'),
+	(3, 'Kibővített felhasználó');
+
+-- Dumping structure for view EventMgrDB.V_AllPeopleWithRole
+DROP VIEW IF EXISTS `V_AllPeopleWithRole`;
+-- Creating temporary table to overcome VIEW dependency errors
+CREATE TABLE `V_AllPeopleWithRole` (
+	`PersonID` INT(10) UNSIGNED NOT NULL,
+	`FirstName` VARCHAR(100) NULL COLLATE 'utf8mb4_general_ci',
+	`LastName` VARCHAR(100) NULL COLLATE 'utf8mb4_general_ci',
+	`Notes` VARCHAR(2000) NULL COMMENT 'Allergy, pereferences, other - free text' COLLATE 'utf8mb4_general_ci',
+	`FK_OrganizationID` INT(10) UNSIGNED NOT NULL,
+	`Role` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`OrgPath` VARCHAR(403) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`OrganizationID` INT(10) UNSIGNED NOT NULL
+) ENGINE=MyISAM;
+
+-- Dumping structure for view EventMgrDB.V_OrgPath
+DROP VIEW IF EXISTS `V_OrgPath`;
+-- Creating temporary table to overcome VIEW dependency errors
+CREATE TABLE `V_OrgPath` (
+	`OrganizationID` INT(10) UNSIGNED NOT NULL,
+	`Name` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`OrgPath` VARCHAR(403) NOT NULL COLLATE 'utf8mb4_general_ci'
+) ENGINE=MyISAM;
+
+-- Dumping structure for view EventMgrDB.V_PersonDetails
+DROP VIEW IF EXISTS `V_PersonDetails`;
+-- Creating temporary table to overcome VIEW dependency errors
+CREATE TABLE `V_PersonDetails` (
+	`PersonID` INT(10) UNSIGNED NOT NULL,
+	`FirstName` VARCHAR(100) NULL COLLATE 'utf8mb4_general_ci',
+	`LastName` VARCHAR(100) NULL COLLATE 'utf8mb4_general_ci',
+	`Notes` VARCHAR(2000) NULL COMMENT 'Allergy, pereferences, other - free text' COLLATE 'utf8mb4_general_ci',
+	`FK_OrganizationID` INT(10) UNSIGNED NOT NULL,
+	`Role` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci'
+) ENGINE=MyISAM;
+
+-- Dumping structure for view EventMgrDB.V_AllPeopleWithRole
+DROP VIEW IF EXISTS `V_AllPeopleWithRole`;
+-- Removing temporary table and create final VIEW structure
+DROP TABLE IF EXISTS `V_AllPeopleWithRole`;
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `V_AllPeopleWithRole` AS select `PD`.`PersonID` AS `PersonID`,`PD`.`FirstName` AS `FirstName`,`PD`.`LastName` AS `LastName`,`PD`.`Notes` AS `Notes`,`PD`.`FK_OrganizationID` AS `FK_OrganizationID`,`PD`.`Role` AS `Role`,`OP`.`OrgPath` AS `OrgPath`,`OP`.`OrganizationID` AS `OrganizationID` from ((`V_OrgPath` `OP` join `PersonToOrganization` `PO` on(`PO`.`FK_OrganizationID` = `OP`.`OrganizationID`)) join `V_PersonDetails` `PD` on(`PO`.`FK_PersonID` = `PD`.`PersonID` and `PO`.`FK_OrganizationID` = `PD`.`FK_OrganizationID`)) order by 3,2;
+
+-- Dumping structure for view EventMgrDB.V_OrgPath
+DROP VIEW IF EXISTS `V_OrgPath`;
+-- Removing temporary table and create final VIEW structure
+DROP TABLE IF EXISTS `V_OrgPath`;
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `V_OrgPath` AS select `O4`.`OrganizationID` AS `OrganizationID`,`O4`.`Name` AS `Name`,concat(`O1`.`Name`,'/',`O2`.`Name`,'/',`O3`.`Name`,'/',`O4`.`Name`) AS `OrgPath` from (((`Organization` `O1` join `Organization` `O2` on(`O2`.`FK_ParentOrganizationID` = `O1`.`OrganizationID`)) join `Organization` `O3` on(`O3`.`FK_ParentOrganizationID` = `O2`.`OrganizationID`)) join `Organization` `O4` on(`O4`.`FK_ParentOrganizationID` = `O3`.`OrganizationID`)) where `O1`.`Toplevel` = 1 union select `O3`.`OrganizationID` AS `OrganizationID`,`O3`.`Name` AS `Name`,concat(`O1`.`Name`,'/',`O2`.`Name`,'/',`O3`.`Name`) AS `OrgPath` from ((`Organization` `O1` join `Organization` `O2` on(`O2`.`FK_ParentOrganizationID` = `O1`.`OrganizationID`)) join `Organization` `O3` on(`O3`.`FK_ParentOrganizationID` = `O2`.`OrganizationID`)) where `O1`.`Toplevel` = 1 union select `O2`.`OrganizationID` AS `OrganizationID`,`O2`.`Name` AS `Name`,concat(`O1`.`Name`,'/',`O2`.`Name`) AS `OrgPath` from (`Organization` `O1` join `Organization` `O2` on(`O2`.`FK_ParentOrganizationID` = `O1`.`OrganizationID`)) where `O1`.`Toplevel` = 1 union select `O1`.`OrganizationID` AS `OrganizationID`,`O1`.`Name` AS `Name`,concat(`O1`.`Name`) AS `OrgPath` from `Organization` `O1` where `O1`.`Toplevel` = 1 order by 1;
+
+-- Dumping structure for view EventMgrDB.V_PersonDetails
+DROP VIEW IF EXISTS `V_PersonDetails`;
+-- Removing temporary table and create final VIEW structure
+DROP TABLE IF EXISTS `V_PersonDetails`;
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `V_PersonDetails` AS select `P`.`PersonID` AS `PersonID`,`P`.`FirstName` AS `FirstName`,`P`.`LastName` AS `LastName`,`P`.`Notes` AS `Notes`,`PO`.`FK_OrganizationID` AS `FK_OrganizationID`,`PO`.`Role` AS `Role` from (`Person` `P` join `PersonToOrganization` `PO` on(`P`.`PersonID` = `PO`.`FK_PersonID`)) order by 1;
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
